0bfad2de297b1525cbb85d15caf956d06c0f040a
AkritiUpadhaya <upadhayaakriti23@gmail.com>
2025-04-18 21:52:19 +0545

    implemented new account UI

--- app/account.tsx
+++ app/account.tsx
@@ -1,9 +1,15 @@
-import { View, Text, StyleSheet} from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Button, TextInput} from 'react-native'
+import React, { useState } from 'react'
 import AccountList from '@/components/AccountList'
 import AccountListItem from '@/components/AccountListItem'
 
 const AccountScreen = () => {
+  const[name, setName]= useState('')
+  const[cap, setCap]= useState('')
+  const[tap, setTap]= useState('')
+  const createAccount=()=>{
+    console.warn("create account", name)
+  }
   return (
     <View style={{gap:5}}>
       {/* <Text>Account</Text> */}
@@ -13,6 +19,12 @@ const AccountScreen = () => {
         <Text>TAP</Text>
       </View>
      <AccountList/>
+     <View style={styles.headingContainer}>
+      <TextInput value={name} onChangeText={setName} placeholder='Name'/>
+      <TextInput value={cap} onChangeText={setCap} placeholder='CAP'/>
+      <TextInput value={cap} onChangeText={setTap} placeholder='TAP'/>
+     </View>
+     <Button title='Add account' onPress={createAccount}/>
     </View>
   )
 }
